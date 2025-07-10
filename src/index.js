6ff0790f62ed5420c058b99074fc416c570ed77b
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2025-07-10 18:21:40 +0300

    addcomit_18.21_10.07.2025
    Методи роботи з файловою системою path та fs.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import path from 'node:path'; // Імпортуємо метод 
+// import path from 'node:path'; // Імпортуємо метод path
+// import fs from 'node:fs' // Імпортуємо метод fs
 
+// Приклад перевірки роботи проекту
+    const message = "Hello, world!";
+    console.log(message);
 
-const message = "Hello, world!";
-console.log(message);
+// Приклад роботи з методами path.join() та process.cwd()
+/*
+    const somePath = path.join('some_folder', 'some_file.txt'); // Створює шлях до файлу
+    const pathToWorkDir = path.join(process.cwd()); // Отримуємо шлях до кореневої директорії
+    const pathToFile = path.join(pathToWorkDir, 'some_folder', 'some_file.txt'); // Вказуємо шлях 
+    console.log(pathToFile);
+*/
 
-const somePath = path.join('some_folder', 'some_file.txt');
-
-const pathToWorkDir = path.join(process.cwd());
-
-const pathToFile = path.join(pathToWorkDir, somePath);
-
-console.log(pathToFile);
-
+/* Операції з файлами за допомогою fs
+    const fileContent = fs.readFileSync('path_to_file'); // Зчитує файл синхронно
+    fs.readFile('path_to_file', (err, fileContent))
+*/
