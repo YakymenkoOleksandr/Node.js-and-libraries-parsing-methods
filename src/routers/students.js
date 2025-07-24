03d970cf3f3ca0a510b1eb3fdce1ad57bb9b600b
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2025-07-24 17:37:00 +0300

    addcomit_17.36_24.07.2025
    Додано коментар.

--- src/routers/students.js
+++ src/routers/students.js
@@ -8,7 +8,7 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js'; // Імпортуємо t
 
 /*Для організації роутингу виділяється папка з файлами де за допомогою методу Router бібліотеки express будуються роути.
 В router передається метод Router(). Замість app. тепер використовуємо router.*/
-const router = Router();
+const router = Router(); // studentsRouter — це екземпляр роутера Express, створений за допомогою функції Router() з пакету express.
 
 //13. Після створення 1. Схеми (new Schema) => 2. Моделі (model("name", nameSchema)) => 3. Сервісу (asycn func + mongoose mathods ) => 4. Створення роуту.
 //13.1 app заміняється на router.
@@ -20,4 +20,7 @@ router.get('/students', ctrlWrapper(getStudentsController));
 //14.2 Другий аргумент, а саме колбек є і називається контролером. І замінюється на контролер.
 router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
 
-export default router;
+/*Тут ти імпортуєш router, який був експортований як default, але називаєш його studentsRouter, бо він містить маршрути,
+пов’язані зі студентами. // А чому тоді імпортується як studentsRouter? Тому що при експорті за замовчуванням 
+(export default) ти можеш назвати імпорт будь-яким іменем, яке хочеш. */
+export default router; 
